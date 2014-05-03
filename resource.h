aafda25b8a3d438062f685d17e85f954d46053e5
Yusuke Ichinohe <yusuke.ichinohe@gmail.com>
2014-05-03 19:24:56 +0900

    Import from CrystalDiskInfo3_5_0Src.zip

--- resource.h
+++ resource.h
@@ -376,13 +376,19 @@
 #define ID_RAW_VALUES_10_ALL            33011
 #define ID_RAW_VALUES_2BYTE             33012
 #define ID_RAW_VALUES_1BYTE             33013
+#define ID_AUTO_DETECTION_05_SEC        33025
+#define ID_AUTO_DETECTION_10_SEC        33026
+#define ID_AUTO_DETECTION_20_SEC        33028
+#define ID_AUTO_DETECTION_30_SEC        33029
+#define ID_AUTO_DETECTION_DISABLE       33030
+#define ID_ASCII_VIEW                   33031
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        146
-#define _APS_NEXT_COMMAND_VALUE         33020
+#define _APS_NEXT_COMMAND_VALUE         33035
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           107
 #endif
